c0b44369cc0eef9fd8d41e7638f55188509c1c01
christoffer.fjellstrom <christoffer.fjellstrom@qlok.se>
2024-01-24 15:08:05 +0100

    Add project files.

---
+++ SimpleTestDemo/MathUtils.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTestDemo
+{
+    public static class MathUtils
+    {
+        public static double GetRectangleArea(double sideA, double sideB)
+        {
+            return sideA * sideB;
+        }
+
+        public static double GetHypotenuse(double a, double b)
+        {
+            double cSquared = a * a + b * b;
+            return Math.Sqrt(cSquared);
+        }
+
+        public static double GetRectangleSide(double area, double side)
+        {
+            if(side == 0)
+            {
+                throw new ArgumentException("Cannot be zero", nameof(side));
+            }
+            return area / side;
+        }
+    }
+}
---
+++ SimpleTestDemoTests/MathUtilsTests.cs
@@ -0,0 +1,81 @@
+﻿using SimpleTestDemo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTestDemoTests
+{
+    [TestClass]
+    public class MathUtilsTests
+    {
+        [TestMethod]
+        public void GetRectangleArea_ReturnsCorrectResult()
+        {
+            // Arrange
+            double sideA = 2;
+            double sideB = 3;
+
+            // Act
+            double result = MathUtils.GetRectangleArea(sideA, sideB);
+
+            // Assert
+            Assert.AreEqual(6, result);
+        }
+
+        [TestMethod]
+        public void GetRectangleArea_ReturnsCorrectResultForNegativeNumbers()
+        {
+            // Arrange
+            double sideA = -4;
+            double sideB = 7;
+
+            // Act
+            double result = MathUtils.GetRectangleArea(sideA, sideB);
+
+            // Assert
+            Assert.AreEqual(-28, result);
+        }
+
+        [TestMethod]
+        public void GetHypotenuse_ReturnsCorrectHyptotenuse()
+        {
+            // Arrange
+            double a = 3;
+            double b = 4;
+
+            // Act
+            double result = MathUtils.GetHypotenuse(a, b);
+
+            // Assert
+            Assert.AreEqual(5, result);
+        }
+
+        [TestMethod]
+        public void GetRectangleSide_ReturnCorrectSideForSimpleRectangle()
+        {
+            // Arrange
+            double area = 4;
+            double side = 2;
+
+            // Act
+            double result = MathUtils.GetRectangleSide(area, side);
+
+            // Assert
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetRectangleSide_ExceptionOnSide0()
+        {
+            // Arrange
+            double area = 4;
+            double side = 0;
+
+            // Act
+            double result = MathUtils.GetRectangleSide(area, side);
+        }
+    }
+}
